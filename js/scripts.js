d37b1f8f591c1b89ee1ebc9c286474253da97172
Alex Fallenstedt and Josh Gustafson <a.fallenstedt@gmail.com>
2016-01-28 11:26:28 -0800

    start our project over with a new branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,45 @@
-//Business Logic
-function Player(mark) {
+////////Business Logic////////
+function Player(mark) {//Creates player with their given mark.
   this.mark = mark;
 }
+var playerOne = new Player("X");
+var playerTwo = new Player("O");
 
-function Space(xcord, ycord) {
-  this.xcord = xcord;
-  this.ycord = ycord;
+function Space(cord) {//the properties of each
+  this.cord = cord;
   this.chosenMark = '';
 }
-
-Space.prototype.markedBy = function(player) {
+Space.prototype.markedBy = function(player) {//marks the space with the player's mark
   this.chosenMark = player.mark;
 }
+//creates 9 new Spaces
+var space1 = new Space("1");
+var space2 = new Space("2");
+var space3 = new Space("3");
+var space4 = new Space("4");
+var space5 = new Space("5");
+var space6 = new Space("6");
+var space7 = new Space("7");
+var space8 = new Space("8");
+var space9 = new Space("9");
+
+var playerOneCord = '';
+var playerTwoCord = '';
+var valueStore = function(space){
+  if (space.chosenMark === 'X') {
+    playerOneCord += space.cord;
+  } else if (space.chosenMark === 'O'){
+    playerTwoCord += space.cord;
+  }
+}
+
+valueStore(space1);
+
+space1.markedBy(playerOne);
+  console.log("This is Space One: " + space1.chosenMark);//tests if this fucking works.
+
+//winning combinations
+
 
 // UI Logic
 $(document).ready(function() {
--- specs/specs.js
+++ specs/specs.js
@@ -1,32 +1,29 @@
-describe ('Board', function() {
-  it("creates a board of 9 spaces when started", function (){
-    var testBoard = new Board()
+describe("valueStore", function(){
+  it("will retrieve the player's chosen cord and store it in their respective variable as a string.", function(){
+    expect(playerOneCord).to.equal("1");
   });
-});
+})
 
 describe("Player", function(){
   it("creates a new player with all of the player's propeties", function(){
     var testPlayer = new Player("X");
     expect(testPlayer.mark).to.equal("X");
   });
+
 });
 describe('Space', function() {
-    it("lets a player mark a space", function() {
-      var testPlayer = new Player("X");
-      var testSpace = new Space(1,2);
-      testSpace.markedBy(testPlayer);
-      expect(testSpace.chosenMark).to.equal('X');
-   });
-    it("returns the player's xcord mark", function() {
-      var testSpace = new Space(1,2);
-      expect(testSpace.xcord).to.equal(1);
-   });
-   it("returns the player's ycord mark", function() {
-     var testSpace = new Space(1,2);
-     expect(testSpace.ycord).to.equal(2);
+  it("lets a player mark a space", function() {
+    var testPlayer = new Player("X");
+    var testSpace = new Space("1");
+    testSpace.markedBy(testPlayer);
+    expect(testSpace.chosenMark).to.equal('X');
+  });
+    it("returns the player's coordinate mark", function() {
+      var testSpace = new Space("1");
+      expect(testSpace.cord).to.equal("1");
   });
   it("returns the player's mark", function() {
-    var testSpace = new Space(1,2);
+    var testSpace = new Space("1");
     expect(testSpace.chosenMark).to.equal("");
- });
+  });
 });
